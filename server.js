96f4709a665f7f92b82225ad741dae7f17308a9f
bamne123 <bamne123@gmail.com>
2018-04-23 12:09:29 +0530

    HTML Message and smtp changed.

--- server.js
+++ server.js
@@ -23,11 +23,18 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     console.log(req.body);
     let transporter = nodeMailer.createTransport({
-        host: 'mail.riderskatta.com',
-        port: 25,
-        secure: false,
+        // host: 'mail.riderskatta.com',
+        // port: 25,
+        // secure: false,
+        // auth: {
+        //     user: 'support@riderskatta.com',
+        //     pass: '4br5xxz1q7n6'
+        // },
+        host: 'advisorsadda.com',
+        port: 465,
+        secure: true,
         auth: {
-            user: 'support@riderskatta.com',
+            user: 'test@advisorsadda.com',
             pass: '4br5xxz1q7n6'
         },
         tls: {
@@ -35,33 +42,51 @@ app.post('/', function (req, res) {
         }
     });
     let mailOptions = {
-        from: '"Support" <support@riderskatta.com>', // sender address
-        // to: 'bamne123@gmail.com', // list of receivers
-        // subject: 'Hi', // Subject line
-        // text: 'Foo', // plain text body
-        // html: '<b>Bar</b>' // html body
+        from: '"Riderskatta Support" <test@advisorsadda.com>', // sender address
         to: req.body.to, // list of receivers
         subject: req.body.subject, // Subject line
-        text: req.body.body, // plain text body
+        // text: req.body.body, // plain text body
         html: `
-            <html xmlns="http://www.w3.org/1999/xhtml">
-            <head>
+        <html xmlns="http://www.w3.org/1999/xhtml">
+
+        <head>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
             <title>Riders Katta</title>
-            </head>
-            <body style="font-family: Calibri, sans-serif; font-size: 16px;">
-            <table style="border:1px solid #ccc; padding:25px;"><tr><td>
-            <div style="width:600px;">
-            <p style="text-align:center;"><img src="http://www.riderskatta.com/logo.png" style="width:150px; height=150px;"><p>
-            <p style="text-align:center; font-size:26px; font-weight:bold; color:orange;">Welcome to RidersKatta.</P>
-            <p style="line-height:1.5; text-align:justify; font-size:18px; ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</P>
-            <br />
-            <p style="line-height:1.5; font-size:15px;">Happy Ride!<br/>RidersKatta Team</p>
-            </div>
-            </td></tr></table>
-            </body>
-            </html>
-        ` // html body
+        </head>
+        
+        <body style="font-family: Calibri, sans-serif; font-size: 16px;">
+            <table style="border:1px solid #ccc; padding:25px;">
+                <tr>
+                    <td>
+                        <div style="width:600px;">
+                            <p style="text-align:center;">
+                                <img src="http://www.riderskatta.com/logo.png" style="width:150px; height=150px;">
+                                <p>
+                                    <p style="text-align:center; font-size:26px; font-weight:bold; color:orange;">Welcome to RidersKatta.</P>
+                                    <p style="line-height:1.5; text-align:justify; font-size:24px; ">
+                                        <b>Thank you for signing up</b>
+                                    </P>
+                                    <p style="line-height:1.5; text-align:justify; font-size:18px; ">I'm absolutely thrilled that you've decided to give RidersKatta a try and
+                                        <br/> just want to you to know that if you want to contact the team,
+                                        <br/> feel free to do so anytime at
+                                        <a href="mailto:support@riderskatta.com">support@riderskatta.com</a>
+                                    </P>
+                                    <p style="line-height:1.5; text-align:justify; font-size:18px; ">The best way to experience RidersKatta is to create lots of rides with your friends.So if you haven't done that already
+                                        you can get started now:</P>
+                                    <br/>
+                                    <p style="line-height:1.5; font-size:15px;">
+                                        <strong>Enjoy the Ride!</strong>
+                                        <br/>RidersKatta Team
+                                        <br/>
+                                        <i>Feel the Throttle...</i>
+                                    </p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </body>
+        
+        </html>`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
